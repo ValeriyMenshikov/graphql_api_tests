f2afd30e533e8862289e7d5378e612d06bfdc0c6
Menshikov Valeriy Sergeevich <vmenshikov@ozon.ru>
2023-07-26 14:43:50 +0300

    fix log

--- graphql_client/__init__.py
+++ graphql_client/__init__.py
@@ -1,7 +1,7 @@
-# import structlog
-#
-# structlog.configure(
-#     processors=[
-#         structlog.processors.JSONRenderer(indent=4, sort_keys=True, ensure_ascii=False)
-#     ]
-# )
+import structlog
+
+structlog.configure(
+    processors=[
+        structlog.processors.JSONRenderer(indent=4, sort_keys=True, ensure_ascii=False)
+    ]
+)
