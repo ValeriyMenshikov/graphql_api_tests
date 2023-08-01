b2bc7e3f2b3d63ce1c5f37a32780be6c6e3675dd
Menshikov Valeriy Sergeevich <vmenshikov@ozon.ru>
2023-08-01 13:51:14 +0300

    message checker

--- tests/graphql/test_logout_all_account.py
+++ tests/graphql/test_logout_all_account.py
@@ -1,7 +1,20 @@
+from generic.checkers.checkers import message_checker
+
+
 def test_logout_all_account(prepare_user, logic):
     login = prepare_user.login
     email = prepare_user.email
     password = prepare_user.password
     logic.account_helper.register_account(login=login, email=email, password=password)
     access_token = logic.account_graphql.login_account(login=login, password=password).token
-    response = logic.account_graphql.logout_all_account(access_token=access_token)
+    logic.account_graphql.logout_all_account(access_token=access_token)
+
+
+def test_logout_all_account_with_invalid_token(prepare_user, logic):
+    login = prepare_user.login
+    email = prepare_user.email
+    password = prepare_user.password
+    logic.account_helper.register_account(login=login, email=email, password=password)
+    access_token = logic.account_graphql.login_account(login=login, password=password).token
+    with message_checker(error_message="Not authorized: ForgedToken") as message:
+        logic.account_graphql.logout_all_account(access_token=access_token + '1')
