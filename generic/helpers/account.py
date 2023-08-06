32c6c91eed5a29e008b87cc659428f64eed3b72c
Menshikov Valeriy Sergeevich <vmenshikov@ozon.ru>
2023-08-06 14:30:23 +0300

    небольшой рефакторинг

--- generic/helpers/account.py
+++ generic/helpers/account.py
@@ -2,14 +2,16 @@ class AccountHelper:
     def __init__(self, logic_provider):
         from generic import LogicProvider
         self.logic_provider: LogicProvider = logic_provider
+        self.graphql_account = self.logic_provider.provider.graphql.dm_api_account
+        self.mailhog_api = self.logic_provider.provider.http.mailhog
 
     def register_account(self, login, email, password):
-        self.logic_provider.provider.graphql.dm_api_account.register_account(
+        self.graphql_account.register_account(
             login=login,
             email=email,
             password=password
         )
-        activation_token = self.logic_provider.provider.http.mailhog.get_token_from_last_email()
-        response = self.logic_provider.provider.graphql.dm_api_account.activate_account(
+        activation_token = self.mailhog_api.get_token_from_last_email()
+        response = self.graphql_account.activate_account(
             activation_token=activation_token)
         return response
